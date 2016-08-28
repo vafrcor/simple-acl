d035610cb35ab74146d52c49cc105865391186c8
Vinsensius <vafrcor2007@yahoo.co.id>
2016-08-28 13:46:03 +0700

    Update: - First commit

---
+++ config/vafrcor_simple_acl.php
@@ -0,0 +1,6 @@
+/**
+ * @Author: montazze
+ * @Date:   2016-08-28 12:43:17
+ * @Last Modified by:   montazze
+ * @Last Modified time: 2016-08-28 12:43:17
+ */
---
+++ src/helpers/common.php
@@ -0,0 +1,31 @@
+<?php
+
+function vfr_simple_acl_get_groups()
+{
+
+}
+
+function vfr_simple_acl_get_roles()
+{
+
+}
+
+function vfr_simple_acl_get_permissions()
+{
+
+}
+
+function vfr_simple_acl_can($module_code='', $action='')
+{
+
+}
+
+function vfr_simple_acl_has_role($role_code='')
+{
+
+}
+
+function vfr_simple_acl_can_view_module($module_code='')
+{
+
+}
